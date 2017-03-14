0dd840ec56de34376b1171e52f6376e5af93d1f7
Jose Luis <sicajancoy@gmail.com>
2017-03-14 14:53:01 -0600

    center principal image

---
+++ MiraclePill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by lion-mane on 3/14/17.
+//  Copyright © 2017 Lionmane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        self.view.backgroundColor = UIColor.purple
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
